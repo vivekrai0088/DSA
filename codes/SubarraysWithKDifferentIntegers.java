e2e35a1bb7a054b72b3897353e21192ef1f5e5a2
Vivek Rai <vivek@Viveks-MacBook-Pro-2.local>
2023-04-21 23:26:36 +0530

    subarrays with different integers

---
+++ codes/SubarraysWithKDifferentIntegers.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class SubarraysWithKDifferentIntegers {
+  public int subarraysWithKDistinct(int[] nums, int k) {
+    return subarraysWithAtmostK(nums, k) - subarraysWithAtmostK(nums, k - 1);
+  }
+
+  private int subarraysWithAtmostK(int[] nums, int k) {
+    int sz = nums.length;
+    int i = 0, j = 0;
+    Map<Integer, Integer> countMap = new HashMap<>();
+    int count = 0;
+    while(j < sz) {
+      countMap.put(nums[j], countMap.getOrDefault(nums[j], 0) + 1);
+
+      while(countMap.size() == k + 1) {
+        countMap.put(nums[i], countMap.get(nums[i]) - 1);
+        if(countMap.get(nums[i]) == 0) {
+          countMap.remove(nums[i]);
+        }
+        i++;
+      }
+      count += (j - i + 1);
+      j++;
+    }
+    return count;
+  }
+}
