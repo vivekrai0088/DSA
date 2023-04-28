4dd94a9c56c0e98a9b4c7656de1df8998f24ec63
Vivek Rai <vivek@Viveks-MacBook-Pro-2.local>
2023-04-28 17:40:08 +0530

    longest consecutive sequence

--- codes/LongestConsecutiveSequence.java
+++ codes/LongestConsecutiveSequence.java
@@ -1,2 +1,29 @@
+import java.util.HashSet;
+import java.util.Set;
+
+// https://leetcode.com/problems/longest-consecutive-sequence/description/
 public class LongestConsecutiveSequence {
+  public int longestConsecutive(int[] nums) {
+    Set<Integer> set = new HashSet<>();
+    int sz = nums.length;
+
+    for(int i = 0; i < sz; i++) {
+      set.add(nums[i]);
+    }
+
+    int maxi = 0;
+    for(int i = 0; i < sz; i++) {
+      int start = nums[i];
+      if(!set.contains(start - 1)) {
+        int ans = 1;
+        start++;
+        while(set.contains(start)) {
+          start++;
+          ans++;
+        }
+        maxi = Math.max(maxi, ans);
+      }
+    }
+    return maxi;
+  }
 }
