84e4a579e9e6794069b313fe1b7d94aa019b129b
Vivek Rai <vivek@Viveks-MacBook-Pro-2.local>
2023-05-17 22:34:03 +0530

    leaf similar trees

---
+++ codes/LeafSimilarTrees.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class LeafSimilarTrees {
+  class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+  }
+  private void rec(TreeNode root, ArrayList<Integer> leafSequence) {
+    if(root == null) return;
+    if(root.left == null && root.right == null) {
+      leafSequence.add(root.val);
+      return;
+    }
+    rec(root.left, leafSequence);
+    rec(root.right, leafSequence);
+  }
+
+  public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+    ArrayList<Integer> firstLeafSequence = new ArrayList<>();
+    ArrayList<Integer> secondLeafSequence = new ArrayList<>();
+
+    rec(root1, firstLeafSequence);
+    rec(root2, secondLeafSequence);
+
+    if(firstLeafSequence.size() != secondLeafSequence.size()) {
+      return false;
+    }
+
+    for(int i = 0; i < firstLeafSequence.size(); i++) {
+      if(!firstLeafSequence.get(i).equals(secondLeafSequence.get(i))) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
