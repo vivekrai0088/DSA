1a3545b07d52a22b1729ed44774dacfbba14e52b
Vivek Rai <vivek@Viveks-MacBook-Pro-2.local>
2023-05-18 20:32:51 +0530

    stock price fluctuation

---
+++ codes/StockPriceFluctuation.java
@@ -0,0 +1,36 @@
+import java.util.TreeMap;
+
+public class StockPriceFluctuation {
+  TreeMap<Integer, Integer> timeseriesMap;
+  TreeMap<Integer, Integer> priceCount;
+
+  public StockPriceFluctuation() {
+    timeseriesMap = new TreeMap<>();
+    priceCount = new TreeMap<>();
+  }
+
+  public void update(int timestamp, int price) {
+    Integer previousPrice = timeseriesMap.get(timestamp);
+    if(previousPrice != null) {
+      priceCount.put(previousPrice, priceCount.getOrDefault(previousPrice, 0) - 1);
+      if(priceCount.get(previousPrice) == 0) {
+        priceCount.remove(previousPrice);
+      }
+    }
+
+    timeseriesMap.put(timestamp, price);
+    priceCount.put(price, priceCount.getOrDefault(price, 0) + 1);
+  }
+
+  public int current() {
+    return timeseriesMap.get(timeseriesMap.lastKey());
+  }
+
+  public int maximum() {
+    return priceCount.lastKey();
+  }
+
+  public int minimum() {
+    return priceCount.firstKey();
+  }
+}
