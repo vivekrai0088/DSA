af2f7ecbbdc9ef049b0bf7562cb97b06777d7f10
Vivek Rai <vivek@Viveks-MacBook-Pro-2.local>
2023-05-18 18:44:06 +0530

    insert interval

---
+++ codes/InsertInterval.java
@@ -0,0 +1,33 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class InsertInterval {
+  public int[][] insert(int[][] intervals, int[] newInterval) {
+    List<int[]> result = new LinkedList<>();
+
+    int i = 0;
+    while(i < intervals.length && intervals[i][1] < newInterval[0]) {
+      result.add(intervals[i]);
+      i++;
+    }
+
+    while(i < intervals.length && intervals[i][0] <= newInterval[1]) {
+      newInterval = new int[]{Math.min(newInterval[0], intervals[i][0]), Math.max(newInterval[1], intervals[i][1])};
+      i++;
+    }
+
+    result.add(newInterval);
+
+    while(i < intervals.length) {
+      result.add(intervals[i]);
+      i++;
+    }
+
+    int[][] ans = new int[result.size()][2];
+    for(int k = 0; k < result.size(); k++) {
+      ans[k][0] = result.get(k)[0];
+      ans[k][1] = result.get(k)[1];
+    }
+    return ans;
+  }
+}
