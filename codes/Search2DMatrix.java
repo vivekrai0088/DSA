d2abd509c51783b5205eeaaa166bcc4af195408f
Vivek Rai <vivek@Viveks-MacBook-Pro-2.local>
2023-05-21 10:55:13 +0530

    search 2d matrix

---
+++ codes/Search2DMatrix.java
@@ -0,0 +1,19 @@
+public class Search2DMatrix {
+  public boolean searchMatrix(int[][] matrix, int target) {
+    int i = 0, j = matrix[0].length - 1;
+
+    while(i < matrix.length && j >= 0) {
+      if(matrix[i][j] == target) {
+        return true;
+      }
+      if(matrix[i][j] > target) {
+        j--;
+      } else {
+        i++;
+      }
+    }
+    return false;
+  }
+
+  // https://leetcode.com/problems/search-a-2d-matrix-ii/description/
+}
